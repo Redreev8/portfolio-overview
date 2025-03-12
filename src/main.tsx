0f2fa7c74cd3b7434a27a001e7775390112f93ab
Redreev8 <redreevs@mail.ru>
2025-03-12 19:50:51 +0500

    add header & logo & btn & container

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import PortfolioOverview from './app/portfolio-overview/portfolio-overview.tsx'
-import Header from './components/header/header.tsx'
+import Header from './components/header'
+import store from './store'
+import './style/globals.scss'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Header />
-        <PortfolioOverview />
+        <Provider store={store}>
+            <Header />
+            <PortfolioOverview />
+        </Provider>
     </StrictMode>,
 )
