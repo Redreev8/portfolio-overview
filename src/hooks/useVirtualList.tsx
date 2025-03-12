a0988f4968bf07cae4d6cf07fa69cdb0a8817215
Redreev8 <redreevs@mail.ru>
2025-03-13 01:02:07 +0500

    add useVirtualList

---
+++ src/hooks/useVirtualList.tsx
@@ -0,0 +1,70 @@
+import { RefObject, useEffect, useLayoutEffect, useRef, useState } from 'react'
+
+interface useVirtualListProps<T extends HTMLElement, A> {
+    arr: Array<A>
+    ref: RefObject<T | null>
+    heightRow: number
+    columns?: number
+}
+
+const useVirtualList = <T extends HTMLElement, A>({
+    arr = [],
+    ref,
+    columns = 1,
+    heightRow,
+}: useVirtualListProps<T, A>) => {
+    const [visibleRows, setVisibleRows] = useState<number>(0)
+    const [position, setPosition] = useState<number>(0)
+    const [virtualArr, setVirtualArr] = useState<A[]>([])
+    const parentRef = useRef<HTMLElement>(null)
+    const handelScroll = () => {
+        const active =
+            Math.floor(parentRef.current!.scrollTop / heightRow) * columns
+        setPosition(
+            Math.min(
+                Math.ceil(arr.length - visibleRows * (columns - 2) * columns),
+                active < 0 ? 0 : active,
+            ),
+        )
+        console.log(
+            Math.min(
+                Math.ceil(arr.length - visibleRows * (columns - 2) * columns),
+                active < 0 ? 0 : active,
+            ),
+        )
+    }
+    useLayoutEffect(() => {
+        if (!ref.current) return
+        parentRef.current = ref.current.parentElement as HTMLElement
+        const parentHeight = parentRef.current.offsetHeight
+        parentRef.current.addEventListener('scroll', handelScroll)
+        setVisibleRows(Math.ceil(parentHeight / heightRow) + 2)
+        console.log(position)
+        return () => {
+            setVisibleRows(0)
+        }
+    }, [])
+
+    const getTopHeight = () => {
+        return heightRow * (position / columns)
+    }
+
+    const getBottomHeight = () => {
+        const rowsNotVisible =
+            arr.length / columns - (position / columns + heightRow)
+        return rowsNotVisible * heightRow
+    }
+
+    useEffect(() => {
+        setVirtualArr(arr.slice(position, position + visibleRows * columns))
+    }, [position, visibleRows])
+
+    return {
+        getTopHeight,
+        getBottomHeight,
+        visibleRows,
+        virtualArr,
+    }
+}
+
+export default useVirtualList
